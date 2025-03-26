a1de7c09fe3748f6e9aef5339da0b775e597896c
Alex <derideveloper>
2025-03-26 15:23:09 +0300

    add login and register pages

---
+++ app/src/main/java/com/example/vibecity/RegisterActivity.java
@@ -0,0 +1,51 @@
+package com.example.vibecity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity {
+    private EditText etName, etEmail, etPassword;
+    private Button btnRegister;
+    private SharedPreferences prefs;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        etName = findViewById(R.id.etName);
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        btnRegister = findViewById(R.id.btnRegister);
+        prefs = getSharedPreferences("AuthPrefs", MODE_PRIVATE);
+
+        btnRegister.setOnClickListener(v -> {
+            String name = etName.getText().toString().trim();
+            String email = etEmail.getText().toString().trim();
+            String password = etPassword.getText().toString().trim();
+
+            if (name.isEmpty() || email.isEmpty() || password.isEmpty()){
+                Toast.makeText(this, "Заполните все поля!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putString("name", name);
+            editor.putString("email", email);
+            editor.putString("password", password);
+            editor.putBoolean("isLoggedIn", true);
+            editor.apply();
+
+            Toast.makeText(this, "Регистрация успешна!", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+            finish();
+        });
+    }
+}
