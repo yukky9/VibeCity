d0dd939ec6ab8857d4788312676ed423a23801d0
Alex <derideveloper>
2025-04-02 14:39:59 +0300

    add map page and  fix

--- app/src/main/java/com/example/vibecity/MapActivity.java
+++ app/src/main/java/com/example/vibecity/MapActivity.java
@@ -1,19 +1,25 @@
 package com.example.vibecity;
 
-import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
+import androidx.annotation.NonNull;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.yandex.mapkit.Animation;
 import com.yandex.mapkit.MapKitFactory;
 import com.yandex.mapkit.geometry.Point;
 import com.yandex.mapkit.map.CameraPosition;
 import com.yandex.mapkit.mapview.MapView;
 
-public abstract class MapActivity extends BaseActivity implements OnMapReadyCallback {
+public class MapActivity extends BaseActivity implements
+        BottomNavigationView.OnNavigationItemSelectedListener {
+
     private MapView mapView;
+    private static final String YANDEX_MAPS_API_KEY = "a44af384-c275-4d80-8b42-909f5d87077b";
+    private static final Point DEFAULT_LOCATION = new Point(55.751574, 37.573856);
 
     @Override
     protected int getLayoutResource() {
@@ -25,29 +31,45 @@ public abstract class MapActivity extends BaseActivity implements OnMapReadyCall
         return R.id.nav_map;
     }
 
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        // Инициализация Яндекс Карт
-        MapKitFactory.setApiKey("ВАШ_API_КЛЮЧ");
+        // Инициализация ДО setContentView!
+        MapKitFactory.setApiKey(YANDEX_MAPS_API_KEY);
         MapKitFactory.initialize(this);
 
-        setContentView(R.layout.activity_map);
-        mapView = findViewById(R.id.mapView);
+        super.onCreate(savedInstanceState);
 
-        // Установка начальной позиции камеры (Москва)
-        Point moscow = new Point(55.751574, 37.573856);
+        // Настройка карты
+        mapView = findViewById(R.id.mapView);
         mapView.getMap().move(
-                new CameraPosition(moscow, 12.0f, 0.0f, 0.0f),
+                new CameraPosition(DEFAULT_LOCATION, 12.0f, 0.0f, 0.0f),
                 new Animation(Animation.Type.SMOOTH, 1f),
                 null
         );
 
-        // Добавление маркера
-        mapView.getMap().getMapObjects().addPlacemark(moscow);
+        // Настройка нижнего меню
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNavigationView);
+        bottomNav.setOnNavigationItemSelectedListener(this);
+        bottomNav.setSelectedItemId(R.id.nav_map);
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.nav_map) {
+            // Уже на карте
+            return true;
+        } else if (id == R.id.nav_profile) {
+            startActivity(new Intent(this, ProfileActivity.class));
+            return true;
+        } else if (id == R.id.nav_events) {
+            startActivity(new Intent(this, MainActivity.class));
+            return true;
+        }
+        return false;
+    }
+
     @Override
     protected void onStart() {
         super.onStart();
